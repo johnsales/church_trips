9f8b172e1cff47f6551019e5fed0ab33e0a114b2
Raul Santos <raul.soutos96@gmail.com>
2020-04-26 16:42:02 -0300

    init project creation

---
+++ src/main/java/com/RJpartnership/church_trips/ChurchTripsApplication.java
@@ -0,0 +1,13 @@
+package com.RJpartnership.church_trips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchTripsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RJpartnership/church_trips/ChurchTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RJpartnership.church_trips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
